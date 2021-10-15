1405294432ceefa647735ecf4fb11dcfa0f2a818
amovfx <amo.vfx@protonmail.com>
2021-10-14 20:54:06 -0600

    prototyped the rendering of markdown

--- flaskserv/book/routes.py
+++ flaskserv/book/routes.py
@@ -1,6 +1,15 @@
 from . import book_bp
 
+import markdown
+import markdown.extensions.fenced_code
+
+import pathlib
+import random
+
 @book_bp.route("/home")
 @book_bp.route("/")
 def home():
-    return "Hello"
+    md_files = [ x.as_posix() for x in pathlib.Path(book_bp.static_folder).rglob("*/*.md")]
+    read_file = open(random.choice(md_files), "r")
+    md_template_string = markdown.markdown(read_file.read(), extensions=["fenced_code"])
+    return md_template_string
