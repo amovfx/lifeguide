a6d737a66c1b10423a5dbfac261add9d9f7c6dc8
amovfx <amovfx@protonmail.com>
2021-10-18 16:22:28 -0600

    changed redirect to go to '/book.page'

--- flaskapp/src/app/error_handlers.py
+++ flaskapp/src/app/error_handlers.py
@@ -7,7 +7,11 @@ error_handler_bp = Blueprint('error_handlers', __name__)
 
 @error_handler_bp.app_errorhandler(404)
 def handle404(e):
-    page_cookie = request.cookies.get('page_num')
-    prev_page = page_cookie if page_cookie else 0
+    """
 
-    return redirect(url_for('/book.page', page_num=prev_page))
+    Missing resources sends us back to the beginning of the book.
+
+    :param e:
+    :return:
+    """
+    return redirect(url_for('/book.page'))
