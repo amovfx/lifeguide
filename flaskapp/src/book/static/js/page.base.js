b430cd18d0e49f6dd644dc14cd5430779c4d3724
amovfx <amovfx@protonmail.com>
2021-10-19 12:17:16 -0600

    updated javascript page managers

--- flaskapp/src/book/static/js/page.base.js
+++ flaskapp/src/book/static/js/page.base.js
@@ -15,21 +15,20 @@ function set_page_data(page_num)
 }
 class PageManagerAbstract
 {
-    constructor(page_val) {
+    constructor(page_count, event_down_name="mousedown", event_up_name="mouseup") {
 
         this.startX;
         this.page_num = 0;
-        this.page_count = page_val;
-        this.init_event_listeners()
+        this.page_count = page_count;
+        this.event_down_name = event_down_name;
+        this.event_up_name = event_up_name;
     }
-
-
 
     turn_page(dX)
     {
         if (Math.abs(dX) > 1)
         {
-            this.page_num += Math.sign(dX);
+            this.page_num -= Math.sign(dX);
             let next = mod((this.page_num), this.page_count);
             set_page_data(next);
         }
@@ -46,9 +45,8 @@ class PageManagerAbstract
 
     init_event_listeners()
     {
-        document.addEventListener('touchdown', this.down_event, false);
-        document.addEventListener('touchup', this.up_event, false );
-
+        document.addEventListener(this.event_down_name, this.down_event, false);
+        document.addEventListener(this.event_up_name, this.up_event, false );
 
         window.addEventListener("load", this.load_event)
         window.load_page = set_page_data
@@ -67,7 +65,8 @@ class PageDesktop extends PageManagerAbstract
 {
     constructor(page_val)
     {
-        super(page_val);
+        super(page_val,"mousedown","mouseup");
+
         //sad ass attempt and overriding
         this.down_event = (event) =>
         {
@@ -77,13 +76,16 @@ class PageDesktop extends PageManagerAbstract
         {
             this.turn_page(event.pageX - this.startX);
         }
+        this.init_event_listeners()
     }
 }
 
 class PageMobile extends PageManagerAbstract
 {
     constructor(page_val) {
-        super(page_val);
+        super(page_val, "touchstart", "touchmove");
+
+        //sad ass attempt at overriding
         this.down_event = (event) =>
         {
             const firstTouch = this.getTouches(event)[0];
@@ -94,10 +96,11 @@ class PageMobile extends PageManagerAbstract
             super.turn_page(event.touches[0].clientX - this.startX);
         }
 
+
     }
     getTouches(event)
     {
       return event.touches ||             // browser API
-             eventt.originalEvent.touches; // jQuery
+             event.originalEvent.touches; // jQuery
     }
 }
