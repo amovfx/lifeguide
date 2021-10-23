02990b6a492cedcc005474599bf668b1c9abca81
amovfx <amovfx@protonmail.com>
2021-10-23 14:46:38 -0600

    formatted with black

--- flaskapp/src/app/__init__.py
+++ flaskapp/src/app/__init__.py
@@ -4,19 +4,30 @@ from flaskapp.src.app.config import *
 
 from .cache import cache
 
-from .config import (DevelopmentConfig,
-                     TestConfig,
-                     ProductionConfig)
+from .config import DevelopmentConfig, TestConfig, ProductionConfig
 
-ENV_CONFIGS = {'development': DevelopmentConfig,
-               'testing': TestConfig,
-               'production': ProductionConfig}
-
-def set_config(app, env_name:str =os.environ.get("FLASK_ENV")) -> None:
+ENV_CONFIGS = {
+    "development": DevelopmentConfig,
+    "testing": TestConfig,
+    "production": ProductionConfig,
+}
 
 
-    if (env_name):
-        if (config_class := ENV_CONFIGS.get(env_name)):
+def set_config(app, env_name: str = os.environ.get("FLASK_ENV")) -> None:
+
+    """
+
+    Set the apps configuration object from the environment variable.
+
+    :param app:
+        Flask app
+    :param env_name:
+        environment variable
+    :return:
+    """
+
+    if env_name:
+        if (config_class := ENV_CONFIGS.get(env_name)) :
             app.config.from_object(config_class)
             return config_class
         else:
@@ -37,17 +48,15 @@ def create_app():
     """
     app = Flask(__name__)
 
-    #set configuration
+    # set configuration
     set_config(app)
 
-
-    #plugins
+    # plugins
     Misaka(app, autolink=True)
-
 
     cache.init_app(app)
 
-    #blueprints
+    # blueprints
     from ..book import book_bp
     from .error_handlers import error_handler_bp
 
