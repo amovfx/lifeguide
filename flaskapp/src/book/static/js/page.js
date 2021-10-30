c06bdb864db961944267fb5e621ea03c62083cf5
amovfx <amovfx@protonmail.com>
2021-10-30 15:12:42 -0600

    Refactor of page manager. Adding ipfs for content management.

--- flaskapp/src/book/static/js/page.js
+++ flaskapp/src/book/static/js/page.js
@@ -5,95 +5,102 @@ function mod(n, m) {
     return ((n % m) + m) % m;
 }
 
-function set_page_data(page_num)
-{
-    fetch(`/book/content/${page_num}`).then
-    (
-        (response) => {
-            response.json().then((data) => {
-                $("#page-contents").html(data);
-            })
-        }
-    )
-    $("#page-number-text").html(page_num);
-}
+class ContentDataManager {
 
-function load_ipfs_book_contents(content_hash)
-{
-    fetch(`https://ipfs.io/ipfs/${content_hash}`)
-        .then((response) =>
-        {
-            response.json().then((data) => {
-                return data;
-            })
-        })
-}
-
-function js_add(a,b)
-{
-    return a + b;
-}
-module.exports = js_add
-
-
-class PageCookieManager
-{
-    getCookie(cname)
+    endpoint_map = {
+        'local' : (page_num) => {return `/book/content/${page_num}`},
+        'ipfs' : (content_hash) => { return`https://ipfs.io/ipfs/${content_hash}`}
+    }
+    constructor(data_source)
     {
-        let name = cname + "=";
-        let ca = document.cookie.split(';');
-        for(let i = 0; i < ca.length; i++)
+        if (data_source in this.endpoint_map)
         {
-            let c = ca[i];
-            while (c.charAt(0) == ' ')
-            {
-              c = c.substring(1);
-            }
-            if (c.indexOf(name) == 0)
-            {
-              return c.substring(name.length, c.length);
-            }
+            this.end_point = this.endpoint_map.get(data_source);
         }
-        return "";
+        else
+        {
+            throw new Error(`Missing endpoint map: ${data_source}`)
+        }
     }
 
+    set_content = (val) => {
+        fetch(this.end_point(val)).then
+        (
+            (response) => {
+                response.json().then((data) => {
+                    $("#page-contents").html(data);
+                })
+            }
+        )
+        $("#page-number-text").html(page_num);
+    }
+}
+
+
+
+/**
+* Cookie manager to get and set the page number from the previous visit.
+*
+* */
+class PageCookieManager
+{
+    PAGE_COOKIE_NAME = "page"
     set_page_cookie_value(page)
     {
-        if (this.getCookie("acceptCookies") == "true")
+        if ($.cookie("acceptCookies") == "true")
         {
-            document.cookie = `page=${page}`;
+            $.cookie(this.PAGE_COOKIE_NAME, page);
         }
     }
     get_page_cookie_value()
     {
-        if (this.getCookie("page"))
+        return $.cookie(this.PAGE_COOKIE_NAME) ? $.cookie(this.PAGE_COOKIE_NAME) : 0;
+
+    }
+}
+
+class PageDataManager
+{
+    constructor(page_count, data_strategy) {
+        this.page_count = page_count;
+        this.Page_Cookie_Manager = new PageCookieManager();
+        this.Data_Strategy = data_strategy;
+        
+        this.page_num = this.Page_Cookie_Manager.get_page_cookie_value();
+        this.init_load_event_listeners()
+    }
+    
+    turn_page(dX)
+    {
+        if (Math.abs(dX) >= delta)
         {
-            return this.getCookie("page")
-        }
-        else
-        {
-            return 0;
+            this.page_num -= Math.sign(dX);
+            let next = mod((this.page_num), this.page_count);
+            this.Data_Strategy(next);
+            this.Page_Cookie_Manager.set_page_cookie_value(next);
         }
     }
+    
+    init_load_event_listeners()
+    {
+        window.addEventListener("load", () => {
+            this.Data_Strategy(this.Page_Cookie_Manager.get_page_cookie_value())
+        })
+    }
+    
 }
 
 
 class PageManagerAbstract
 {
-    constructor(page_count, event_down_name="mousedown", event_up_name="mouseup")
+    constructor(page_count, PageTurnStrat)
     {
         this.startX;
         this.Page_Cookie_Manager = new PageCookieManager();
+        this.Page_Turn_Strategy = new PageTurnStrat();
         this.page_num = this.Page_Cookie_Manager.get_page_cookie_value();
         this.page_count = page_count;
-        this.event_down_name = event_down_name;
-        this.event_up_name = event_up_name;
 
-    }
-
-    get_page_num()
-    {
-        return this.page_num;
     }
 
     turn_page(dX)
@@ -106,26 +113,91 @@ class PageManagerAbstract
             this.Page_Cookie_Manager.set_page_cookie_value(next);
         }
     }
-    down_event(event)
-    {
-        alert("Not Implimented");
-    }
 
-    up_event(event)
-    {
-        alert("Not implimented");
-    }
-
-    init_event_listeners()
-    {
-        document.addEventListener(this.event_down_name, this.down_event, false);
-        document.addEventListener(this.event_up_name, this.up_event, false );
-    }
     init_load_event_listeners()
     {
         window.addEventListener("load", () => {
             set_page_data(this.Page_Cookie_Manager.get_page_cookie_value())
         })
+    }
+}
+
+class EventStrategy
+{
+    constructor(event_name_down, event_name_up, dataManager_function)
+    {
+        this.event_name_down = event_name_down;
+        this.event_name_up = event_name_up;
+        this.fn = dataManager_function
+    }
+
+}
+
+class EventStrategyDesktop extends EventStrategy
+{
+    down_event = (event) =>
+    {
+        this.startX = event.pageX;
+    }
+    up_event = (event) =>
+    {
+        this.fn(event.pageX - this.startX);
+    }
+}
+
+class EventStrategyMobile extends EventStrategy
+{
+    down_event = (event) =>
+    {
+        this.startX = event.changedTouches[0].screenX;
+    }
+    up_event = (event) =>
+    {
+        this.fn(event.changedTouches[0].screenX - this.startX);
+    }
+}
+
+function CreatePageTurnEventListeners(strategy)
+{
+        document.addEventListener(strategy.event_name_down
+            , (event) => {strategy.down_event()}
+            , false);
+        document.addEventListener(strategy.event_name_up
+            , (event) => {strategy.up_event()}
+            , false);
+}
+
+class PageManager
+{
+    constructor(event_strategy, data_manager)
+    {
+        self.event_strategy = event_strategy;
+        self.data_manager = data_manager;
+        
+        self.cookie_manager = new PageCookieManager();
+    }
+    
+    
+}
+
+function CreatePageLoadEventListener()
+
+class PageTurnEventListeners
+{
+    constructor(strategy) {
+        this.strategy = strategy;
+        this.init_event_listeners()
+    }
+
+    init_event_listeners()
+    {
+        document.addEventListener(this.strategy.event_name_down
+            , (event) => {this.strategy.down_event()}
+            , false);
+        document.addEventListener(this.strategy.event_name_up
+            , (event) => {this.strategy.up_event()}
+            , false);
+
     }
 }
 
@@ -156,7 +228,6 @@ class PageMobile extends PageManagerAbstract
         //sad ass attempt at overriding
         this.down_event = (event) =>
         {
-            console.log("poopy")
             this.startX = event.changedTouches[0].screenX;
         }
         this.up_event = (event) =>
@@ -167,4 +238,11 @@ class PageMobile extends PageManagerAbstract
     }
 
 }
+
+
+function js_add(a,b)
+{
+    return a + b;
+}
+module.exports = js_add
 
