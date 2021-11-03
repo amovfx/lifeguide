63b635e2876167ce11b5a00facfb80b0d5723817
amovfx <amovfx@protonmail.com>
2021-11-03 14:45:28 -0600

    Removed experiments and notes

--- flaskapp/src/book/static/js/page.test.js
+++ flaskapp/src/book/static/js/page.test.js
@@ -1,41 +1,9 @@
 //const js_add = require("./page")
-import {IPFSBook, js_add, IPFS_Data_Resolver } from "./page.js"
+import {IPFSBook, js_add, IPFS_Data_Resolver, Data_Resolver } from "./page.js"
 
 const axios = require('axios').default;
 
-beforeEach(() => {
-    //run this code before each
-    //setup data before tests
-})
-
-afterEach(() => {
-    //tear down
-})
-
-beforeAll(async () => {
-    //set up once
-    const response = await axios.get('https://ipfs.io/ipfs/QmXY68cNw16ASk2crFRG2nv6GVU8AaSfrwr9wGosqsgW8R')
-})
-
-afterAll(() => {
-    //tear down after all
-})
-
-describe("test_basket", () => {
-
-
-
-    it("my_test", () => {
-        //calculate result
-        let result = 5;
-        expect(result).toEqual(5);
-    })
-    it("test_module_export", () => {
-        let result = js_add(5,6);
-        expect(result).toEqual(11);
-    })
-})
-
+jest.setTimeout(15000)
 describe("Testing Data Resolvers", () =>
 {
     let ipfs_endpoint = 'QmXY68cNw16ASk2crFRG2nv6GVU8AaSfrwr9wGosqsgW8R'
@@ -43,8 +11,7 @@ describe("Testing Data Resolvers", () =>
     {
         let ipfs_data_resolver;
 
-        it("Initializing IPFS_Data_Resolver", () =>
-        {
+        it("Initializing IPFS_Data_Resolver", () => {
             ipfs_data_resolver = new IPFS_Data_Resolver();
             expect(ipfs_data_resolver).toBeInstanceOf(IPFS_Data_Resolver)
             expect(ipfs_data_resolver.endpoint).toBe("https://ipfs.io/ipfs/")
@@ -56,6 +23,21 @@ describe("Testing Data Resolvers", () =>
             expect(response.status).toBe(200)
             expect(response.data[0]['Intro.01.md']).toBe("QmTDnfTQ37682djSgujCDhaW4k9Fw4ZdtBJHZpqyQfXwfr")
 
+        })
+
+    })
+})
+
+describe("Testing centralized data resolvers", () =>
+{
+    let centralized_endpoint = "localhost:5000";
+    describe("Data Resolver", () =>
+    {
+        let data_resolver = new Data_Resolver(centralized_endpoint);
+        it("Initialization", () =>
+        {
+            expect(data_resolver).toBeInstanceOf(Data_Resolver);
+            expect(data_resolver.endpoint).toBe(centralized_endpoint);
         })
 
     })
@@ -85,47 +67,3 @@ describe("Testing IPFS Book", () =>
         expect(ipfs_book).isPrototypeOf(IPFSBook);
     })
 })
-
-//an example of mocking data
-const forEach = (items, callback) => {
-    for (let i = 0; i < items.length; i ++)
-    {
-        callback(items[i]);
-    }
-}
-
-
-it("mock callback", () => {
-    const mockCalledback = jest.fn(x => 741 + x);
-    forEach([0,1], mockCalledback);
-    expect(mockCalledback.mock.calls.length).toBe(2);
-    expect(mockCalledback.mock.calls[0][0]).toBe(0);
-    expect(mockCalledback.mock.results[0].value).toBe(741);
-})
-
-it("mock return", () => {
-    const mock = jest.fn();
-
-    mock.mockReturnValueOnce(false)
-        .mockReturnValueOnce(true)
-        .mockReturnValueOnce("hello");
-
-    let results1 = mock();
-    let results2 = mock();
-    let results3 = mock();
-
-    expect(results1).toBe(false);
-    expect(results2).toBe(true);
-    expect(results3).toBe("hello");
-})
-
-// it ("mock axios", async () => {
-//     jest.spyOn(axios, "get").mockReturnValueOnce({
-//         data: {
-//             id: 1,
-//             todo: "Get 1m subs"
-//         }
-//     });
-//     const results = await fetchData(1);
-//     expect(results.todo).toBe("Get 1m subs");
-// })
