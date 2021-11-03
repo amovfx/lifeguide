d52a68f697eb2fb29b17391905121e085bbc2cf4
amovfx <amovfx@protonmail.com>
2021-11-03 12:40:02 -0600

    Fixed up linting errors and doc strings.

--- flaskapp/src/book/tests/test_routes.py
+++ flaskapp/src/book/tests/test_routes.py
@@ -1,26 +1,31 @@
 """
 
-Testing for the book_bp
-
-Notes:
-    -Convert output to the test to a string
-    -Have an expected string
-    -Every Test works the same way
+Testing for the book_bp routes
 
 """
 
-from ddt import ddt, idata, unpack
 import json
+
+from ddt import ddt, idata, unpack
 from tabulate import tabulate
+
 from ...app.tests.test import TestBaseCase
 from .. import book_bp
 
 
-
-
 def annotate(expected, actual):
+    """
+
+    Output formatter for a test.
+
+    :param expected:
+        Expected string
+    :param actual:
+        Actual data in string form that the test returns
+    """
     msg = tabulate([["Expected:", expected], ["Actual: ", actual]])
     print(msg)
+
 
 @ddt
 class TestRoutes(TestBaseCase):
@@ -29,10 +34,6 @@ class TestRoutes(TestBaseCase):
     Test routes of the book_bp
 
     """
-
-
-
-
 
     def test_home(self):
         """
@@ -44,34 +45,35 @@ class TestRoutes(TestBaseCase):
 
         """
 
-        response = self.client.get('/', content_type='html/text')
+        response = self.client.get("/", content_type="html/text")
         annotate("response.status_code = 302", f"{response.status_code = }")
-        annotate("response.location = 'http://localhost/book/'", f'{response.location = }')
+        annotate(
+            "response.location = 'http://localhost/book/'", f"{response.location = }"
+        )
 
         self.assertEqual(302, response.status_code)
-        self.assertRedirects(response, '/book/')
-
+        self.assertRedirects(response, "/book/")
 
     @idata(range(5))
     def test_badroutes(self, value):
         """
 
         Tests reroute with error handles to the book page.
-        :return:
+
         """
-        self.longMessage = True
-        response = self.client.get(f'/{value}', content_type='html/text')
+
+        response = self.client.get(f"/{value}", content_type="html/text")
         self.assertEqual(302, response.status_code)
-        self.assertRedirects(response, '/book/')
+        self.assertRedirects(response, "/book/")
 
     def test_cover(self):
         """
 
         test direct cover route.
 
-        :return:
+
         """
-        response = self.client.get('/book/', content_type='html/text')
+        response = self.client.get("/book/", content_type="html/text")
         self.assertEqual(200, response.status_code)
 
     @idata(range(len(book_bp.files)))
@@ -80,9 +82,10 @@ class TestRoutes(TestBaseCase):
 
         Test for testing book page. This is going to need to be redone.
         :param value:
-        :return:
+            tests each page.
+
         """
-        response = self.client.get(f'/book/content/{value}', content_type='html/text')
+        response = self.client.get(f"/book/content/{value}", content_type="html/text")
         self.assertEqual(200, response.status_code)
 
     @idata(range(5))
@@ -91,13 +94,13 @@ class TestRoutes(TestBaseCase):
 
         Test for testing book page. This is going to need to be redone.
             :param value:
-        :return:
+
         """
         value += len(book_bp.files) + 1
-        response = self.client.get(f'/book/content/{value}', content_type='html/text')
+        response = self.client.get(f"/book/content/{value}", content_type="html/text")
         self.assertEqual(404, response.status_code)
 
-    @idata(zip(('Intro.01.md', 'Autonomy.02.md', 'current_mindset.03.md'), range(3)))
+    @idata(zip(("Intro.01.md", "Autonomy.02.md", "current_mindset.03.md"), range(3)))
     @unpack
     def test_book_contents(self, key, value):
         """
@@ -105,15 +108,12 @@ class TestRoutes(TestBaseCase):
         Test for testing book table of contents route.
 
         :param value:
-        :return:
+
         """
-        print('\n')
-        print(f'Testing book contents {value}...')
-        response = self.client.get('/book/contents', content_type='json')
+        print("\n")
+        print(f"Testing book contents {value}...")
+        response = self.client.get("/book/contents", content_type="json")
         first_entry = json.loads(response.data)[value]
         self.assertIn(key, first_entry)
-        self.assertEqual(f'/book/content/{value}', first_entry[key])
-        print(f'Expected: /book/content/{value} Acctual: {first_entry[key]}')
-
-
-
+        self.assertEqual(f"/book/content/{value}", first_entry[key])
+        print(f"Expected: /book/content/{value} Acctual: {first_entry[key]}")
