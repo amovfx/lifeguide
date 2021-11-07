bfaa672a50edc35aec904e2527fbe0b286e1b125
andrewoseen <amo.vfx@protonmail.com>
2021-11-07 13:03:15 -0700

    testing page

--- flaskapp/src/book/static/js/page.test.js
+++ flaskapp/src/book/static/js/page.test.js
@@ -1,4 +1,4 @@
-//const js_add = require("./page")
+
 import {  Data_Resolver, Page, Book, Table_of_Contents } from "./page.js"
 
 const axios = require('axios').default;
@@ -13,21 +13,39 @@ jest.setTimeout(50000)
 
 describe("Testing centralized Data_Resolver", () =>
 {
-        describe("Testing Table of Contents", () => {
+    describe("Testing Table of Contents", () => {
 
-            it ("Table of Contents (local)", async () =>
-            {
-                let table_of_contents = await Table_of_Contents.local();
-                expect(table_of_contents.chapters[0]).toMatchObject({"Intro.01.md": "/book/content/0"})
-            })
+        it ("Table of Contents (local)", async () =>
+        {
+            let table_of_contents = await Table_of_Contents.local();
+            expect(table_of_contents.chapters[0]).toMatchObject({"Intro.01.md": "/book/content/0"})
+        })
 
-            it ("Table of Contents (ipfs)", async () =>
-            {
-                let table_of_contents = await Table_of_Contents.ipfs();
-                expect(table_of_contents.chapters[0]).toMatchObject({"Intro.01.md": "QmTDnfTQ37682djSgujCDhaW4k9Fw4ZdtBJHZpqyQfXwfr"})
-            })
+        it ("Table of Contents (ipfs)", async () =>
+        {
+            let table_of_contents = await Table_of_Contents.ipfs();
+            expect(table_of_contents.chapters[0]).toMatchObject({"Intro.01.md": "QmTDnfTQ37682djSgujCDhaW4k9Fw4ZdtBJHZpqyQfXwfr"})
+        })
+
+    })
+
+    describe("Testing Book",  () => {
+
+
+        it ("Testing Page", async () => {
+            let table_of_contents = await Table_of_Contents.local();
+            let new_page = table_of_contents.get_page(0);
+            expect(new_page.get_title()).toBe("Intro");
+            expect(new_page.get_page_num()).toBe(0);
+        })
+
+        it ("Testing Page Datta", () => {
 
         })
+
+
+    })
+
 
 
 })
