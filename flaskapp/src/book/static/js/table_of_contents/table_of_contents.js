9dc7b42d0ed34abbdff11825be8f224013743d7b
amovfx <amovfx@protonmail.com>
2021-11-08 15:46:53 -0700

    Seperating out tests and refactoring module location.

--- flaskapp/src/book/static/js/table_of_contents/table_of_contents.js
+++ flaskapp/src/book/static/js/table_of_contents/table_of_contents.js
@@ -1,40 +1,34 @@
 
 
-import Data_Resolver from "./data_resolver";
+import Data_Resolver from "../data_resolver/data_resolver";
 
-import Page from "./page";
 
 export default class Table_of_Contents
 {
-    constructor(chapters, resolver) {
-        this.chapters = chapters;
-        this.resolver = resolver;
-    }
-
-    static async local()
+    static async Local()
     {
         let local_resolver = Data_Resolver.Local_Resolver()
         let data = await local_resolver.async_load();
         return new Table_of_Contents(data, local_resolver);
     }
 
-    static async ipfs()
+    static async IPFS()
     {
         let ipfs_resolver = Data_Resolver.IPFS_Resolver()
         let data = await ipfs_resolver.async_load();
         return new Table_of_Contents(data, ipfs_resolver);
     }
 
-    static async web2()
+    static async Web2()
     {
         let web2_resolver = Data_Resolver.Web2_Resolver();
         let data = await web2_resolver.async_load();
         return new Table_of_Contents(data, web2_resolver);
     }
 
-    build_page(index)
-    {
-        return new Page(this.resolver, this.chapters[index]);
+    constructor(chapters, resolver) {
+        this.chapters = chapters;
+        this.resolver = resolver;
     }
 
     count()
