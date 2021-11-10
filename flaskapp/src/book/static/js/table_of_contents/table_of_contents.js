febcea62d1829a6a796f0b77ca8d0c44c74f9267
amovfx <amovfx@protonmail.com>
2021-11-10 14:11:06 -0700

    Table of contents extends array. Removed static constructors for build.

--- flaskapp/src/book/static/js/table_of_contents/table_of_contents.js
+++ flaskapp/src/book/static/js/table_of_contents/table_of_contents.js
@@ -3,36 +3,17 @@
 import Data_Resolver from "../data_resolver/data_resolver";
 
 
-export default class Table_of_Contents
+export default class Table_of_Contents extends Array
 {
-    static async Local()
+    static async build(domain)
     {
-        let local_resolver = Data_Resolver.Local_Resolver()
-        let data = await local_resolver.async_load();
-        return new Table_of_Contents(data, local_resolver);
+        let resolver = Data_Resolver.from_domain(domain);
+        let data = await resolver.async_load();
+        return new Table_of_Contents(data, domain)
     }
 
-    static async IPFS()
-    {
-        let ipfs_resolver = Data_Resolver.IPFS_Resolver()
-        let data = await ipfs_resolver.async_load();
-        return new Table_of_Contents(data, ipfs_resolver);
-    }
-
-    static async Web2()
-    {
-        let web2_resolver = Data_Resolver.Web2_Resolver();
-        let data = await web2_resolver.async_load();
-        return new Table_of_Contents(data, web2_resolver);
-    }
-
-    constructor(chapters, resolver) {
-        this.chapters = chapters;
-        this.resolver = {...resolver};
-    }
-
-    count = () =>
-    {
-        return this.chapters.length;
+    constructor(chapters, resolver_type) {
+        super([...chapters]);
+        this.resolver_type = resolver_type;
     }
 }
