6348fe4a158a60a517e116c2512c990137512e51
andrewoseen <amo.vfx@protonmail.com>
2021-12-20 14:59:38 -0700

    adding ssl context

--- flaskapp/main.py
+++ flaskapp/main.py
@@ -4,4 +4,4 @@ from src.app_factory import create_app
 application = create_app()
 
 if __name__ == "__main__":
-    application.run(host="0.0.0.0")
+    application.run(ssl_context='adhoc', host="0.0.0.0")
