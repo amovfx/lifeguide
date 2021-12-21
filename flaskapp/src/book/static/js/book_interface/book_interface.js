1c8ddd4d8afacb595313372e1ab6ba452f867f09
andrewoseen <amo.vfx@protonmail.com>
2021-12-21 11:50:35 -0700

    fixed calls to set_page_data

--- flaskapp/src/book/static/js/book_interface/book_interface.js
+++ flaskapp/src/book/static/js/book_interface/book_interface.js
@@ -24,7 +24,7 @@ export class BookInterface
     {
         this.book = book;
         this.set_contents()
-        this.set_page_data()
+        this.set_page_data(this.Bookmark.get_page_number())
     }
     set_contents = () =>
     {
@@ -48,24 +48,24 @@ export class BookInterface
             current_page -= Math.sign(dX);
             current_page = mod(current_page, this.book.pages.length);
             this.Bookmark.set_page_number(current_page);
+            this.set_page_data(current_page);
         }
-        this.set_page_data();
+
     }
     goto_page(page_num)
     {
         page_num = mod(page_num, this.book.pages.length);
         this.Bookmark.set_page_number(page_num);
-        this.set_page_data();
+        this.set_page_data(page_num);
     }
 
     //strip this out to a renderer class.
-    set_page_data()
+    set_page_data(page_num)
     {
         if (this.book !== undefined)
         {
-            let page_number = this.Bookmark.get_page_number();
-            let page = this.book.get_page(page_number);
-            this.MenuManager.set_active_menu_item(page_number);
+            let page = this.book.get_page(page_num);
+            this.MenuManager.set_active_menu_item(page_num);
             render_page(page);
         }
         else
