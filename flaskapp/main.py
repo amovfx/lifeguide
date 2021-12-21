97ede805c2bdb04fd2e7af70f8d303d5f9fda96a
andrewoseen <amo.vfx@protonmail.com>
2021-12-21 11:42:02 -0700

    run application with ssl when not in development.

--- flaskapp/main.py
+++ flaskapp/main.py
@@ -1,7 +1,12 @@
+import os
 from src.app_factory import create_app
 
 
 application = create_app()
 
 if __name__ == "__main__":
-    application.run(ssl_context='adhoc', host="0.0.0.0")
+    if os.getenv("FLASK_ENV") == 'development':
+        application.run()
+    else:
+        application.run(ssl_context='adhoc', host="0.0.0.0")
+
