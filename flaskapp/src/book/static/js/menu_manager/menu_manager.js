aec0bb49c8f5e77bb9837b3875e4b065d5323660
andrewoseen <amo.vfx@protonmail.com>
2021-12-29 12:55:42 -0700

    Refactored menu manager into MenuBuilder

--- flaskapp/src/book/static/js/menu_manager/menu_manager.js
+++ flaskapp/src/book/static/js/menu_manager/menu_manager.js
@@ -2,31 +2,6 @@ import {Page, PageManager} from "../page/page";
 import {Book} from "../book/book";
 import Logger from "js-logger";
 
-function create_menu_category(name, category)
-{
-    Logger.info(`Creating menu category : ${name}`)
-    let category_text = document.createElement("span");
-    category_text.innerHTML = name;
-
-    let category_div = document.createElement("div");
-    category_div.classList.add("menu-cat");
-    category_div.append(category_text);
-
-    return category_div
-}
-
-function create_menu_element(name)
-{
-    Logger.info(`Creating menu element : ${name}`)
-    let menu_text = document.createElement("p");
-    menu_text.innerHTML = name;
-
-    let menu_div = document.createElement("div");
-    menu_div.classList.add("menu-item");
-    menu_div.append(menu_text);
-
-    return menu_div;
-}
 
 function format_title(title)
 {
@@ -35,12 +10,13 @@ function format_title(title)
     return capitalized_title;
 }
 
-export class MenuManager
+export class MenuBuilder
 {
     constructor()
     {
         this.last_item = undefined;
         this.sidebar_element = document.getElementById("page-sidebar-contents")
+
 
     }
 
@@ -54,10 +30,12 @@ export class MenuManager
         category_div.classList.add("menu-cat");
         category_div.append(category_text);
 
+        category.append(category_div);
+
         return category_div
     }
 
-    create_menu_element(name)
+    create_menu_element(name, index)
     {
         Logger.info(`Creating menu element : ${name}`)
         let menu_text = document.createElement("p");
@@ -65,45 +43,20 @@ export class MenuManager
 
         let menu_div = document.createElement("div");
         menu_div.classList.add("menu-item");
+        menu_div.onclick = () =>
+        {
+            Logger.info(`Custom event: ${index}`);
+            const event = new CustomEvent('set_page',
+            {
+                detail: {
+                    page: index,
+                }
+            })
+            menu_div.dispatchEvent(event);
+        }
         menu_div.append(menu_text);
 
         return menu_div;
-    }
-
-    static format_title(title)
-    {
-        let formatted_title = title.replace('_',' ');
-        let capitalized_title = formatted_title.charAt(0).toUpperCase() + formatted_title.slice(1)
-        return capitalized_title;
-    }
-
-    setfirst
-
-    async initialize_menu(pages, book_interface)
-    {
-        pages.forEach((item, index) =>
-        {
-            let title = format_title(item.get_title());
-            let new_item = create_menu_element(title);
-
-            if (index === 0)
-            {
-                this.last_item = new_item;
-                new_item.classList.toggle("menu-active");
-            }
-
-            new_item.onclick = () =>
-            {
-                const event = new CustomEvent('set_page',
-                {
-                    detail: {
-                        page: index,
-                    }
-                })
-                new_item.dispatchEvent(event);
-            }
-            this.sidebar_element.append(new_item);
-        })
     }
 
     set_active_menu_item(page_num)
@@ -116,7 +69,7 @@ export class MenuManager
         let menu_item = this.sidebar_element.children[page_num];
         menu_item.classList.toggle("menu-active");
         this.last_item = menu_item;
-        Logger.info("Running for some shit for  yo webpack");
+        Logger.info("Bufddfdsfsd refresh");
     }
 
 }
