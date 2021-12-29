c349f2e78cadc5fe592367e5f261e8a8e897b95d
andrewoseen <amo.vfx@protonmail.com>
2021-12-29 15:18:15 -0700

    Renamed BookInterface CBookInterface

--- flaskapp/src/book/static/js/book_interface/book_interface.js
+++ flaskapp/src/book/static/js/book_interface/book_interface.js
@@ -1,7 +1,7 @@
 import Logger from "js-logger";
-import {Bookmark} from "../bookmark/bookmark";
+import {CBookmark} from "../bookmark/bookmark";
 
-import {buildMenu, MenuManager} from "../menu_manager/menu_manager";
+import {buildMenu, MenuBuilder} from "../menu_manager/menu_manager";
 
 
 export const DELTA = 6;
@@ -10,58 +10,16 @@ function mod(n, m) {
     return ((n % m) + m) % m;
 }
 
-export class Book
-{
-    constructor(menuman, pageman)
-    {
-        this.MenuManager = menuman;
-        this.PageManager = pageman;
-        this.Bookmark = new Bookmark();
 
-        //this listens to the events that menu manager broadcasts
-        document.addEventListener('set_page', (e) =>
-        {
-            this.set_page(e.detail.page);
-        })
-
-    }
-
-    set_book(book)
-    {
-        this.book = book;
-        this.MenuManager.render_menu();
-        this.set_page(this.Bookmark.get_page_number())
-    }
-
-    turn_page(dX)
-    {
-        let current_page = this.Bookmark.get_page_number();
-        if (Math.abs(dX) >= DELTA)
-        {
-            current_page -= Math.sign(dX);
-            current_page = mod(current_page, this.book.pages.length);
-            this.set_page(current_page);
-        }
-    }
-
-    set_page(page_num)
-    {
-        page_num = mod(page_num, this.book.pages.length);
-        this.current_page = page_num;
-        this.Bookmark.set_page_number(page_num);
-        this.MenuManager.set_active_menu_item(page_num);
-        this.PageManager.render_page(page_num);
-    }
-}
 
 //rename this book
-export class BookInterface
+export class CBookInterface
 {
     constructor(book)
     {
         Logger.info("Constructing lifeguide interface");
         this.book = book;
-        this.Bookmark = new Bookmark();
+        this.Bookmark = new CBookmark();
     }
 
     set_book = (book) =>
@@ -81,7 +39,6 @@ export class BookInterface
         if (Math.abs(dX) >= DELTA)
         {
             current_page -= Math.sign(dX);
-            current_page = mod(current_page, this.book.pages.length);
             this.Bookmark.set_page_number(current_page);
             this.set_page_data(current_page);
         }
@@ -89,7 +46,6 @@ export class BookInterface
     }
     goto_page(page_num)
     {
-        page_num = mod(page_num, this.book.pages.length);
         this.Bookmark.set_page_number(page_num);
         this.set_page_data(page_num);
     }
