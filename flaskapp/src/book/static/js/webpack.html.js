3620c0f360428dc48f1c3eac316ffb70171b0af8
andrewoseen <amo.vfx@protonmail.com>
2022-01-08 15:17:03 -0700

    Adding packhtml tool

---
+++ flaskapp/src/book/static/js/webpack.html.js
@@ -0,0 +1,10 @@
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+const fs = require('fs');
+
+const html_files = fs.readdirSync('../templates')
+    .reduce((acc, v) => (
+        { ...acc, [v.split(".")[0]]: `../html/${v}` }
+    ), {});
+
+console.log(html_files)
