ce261e2e74281e1ecf9bc6d703c9717a96f7a838
andrewoseen <amo.vfx@protonmail.com>
2022-01-08 17:13:27 -0700

    Formatting and comments. get_html_files is now generalized and takes an extension argument.

--- flaskapp/src/utils/hash_html_templates.py
+++ flaskapp/src/utils/hash_html_templates.py
@@ -1,4 +1,3 @@
-
 """
 
 Quick utility to copy html files for cache busting.
@@ -8,6 +7,8 @@ import hashlib
 import pathlib
 import shutil
 
+CWD = pathlib.Path.cwd()
+
 def clean_old_files():
     """
 
@@ -15,23 +16,29 @@ def clean_old_files():
 
     :return:
     """
-    dist_path = pathlib.Path.cwd() / 'dist'
+    dist_path = CWD / "dist"
     for file in dist_path.rglob("*html"):
         file.unlink()
 
 
-def get_html_files():
+def get_files(ext="html"):
     """
 
-    Get a recusive glob of all html files in a working directory.
+    Get a recursive glob of all html files in a working directory.
+
     :return:
     """
-    return list(pathlib.Path(pathlib.Path.cwd()).rglob("*html"))
+    files = list(pathlib.Path(CWD).rglob(f"*{ext}"))
+
+    if not len(files):
+        raise FileNotFoundError(f"No html files found in {CWD}")
+    return files
+
 
 def generate_digest(file: pathlib.Path) -> str:
     """
 
-    read contents of the file and return a 16 character length md5 hash.
+    Read contents of the file and return a 16 character length md5 hash.
 
     :param file:
         pathlib.Path object of an html file
@@ -45,8 +52,7 @@ def generate_digest(file: pathlib.Path) -> str:
     return digest[:16]
 
 
-
-def hashed_filename(file : pathlib.Path) -> str:
+def hashed_filename(file: pathlib.Path) -> str:
     """
 
     create a file name of format filename.[md5hash].[ext]
@@ -58,22 +64,41 @@ def hashed_filename(file : pathlib.Path) -> str:
     path = ".".join([file.stem, generate_digest(file)]) + file.suffix
     return path
 
-def digested_file_output_path(file : pathlib.Path) -> pathlib.Path:
+
+def digested_file_output_path(file: pathlib.Path) -> pathlib.Path:
     """
 
     this is out outputpath
     :param file:
     :return:
     """
-    return pathlib.Path.cwd() / 'dist' / hashed_filename(file)
+    return CWD / "dist" / hashed_filename(file)
 
-def write_digested_file(file):
+
+def write_digested_file(file: pathlib.Path):
+    """
+
+    Write file.html to file.[md5hash].html
+
+    :param file:
+    :return:
+    """
     shutil.copy2(file, digested_file_output_path(file))
 
+
 def main():
+    """
+
+    Get all html files in all sub directories in current working directory
+    and copy them to fiels with the hash in the extension.
+
+    :return:
+    """
+
     clean_old_files()
-    for file in get_html_files():
+    for file in get_files():
         write_digested_file(file)
+
 
 if __name__ == "__main__":
     main()
